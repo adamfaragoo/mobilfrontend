3032abf09b2a6e69b10315b56489cffc5cbbe39a
adamfaragoo <adam.farago004@gmail.com>
2021-12-10 13:46:19 +0100

    asd

--- Kereses.js
+++ Kereses.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
-import { Text, TextInput, View, FlatList, Image, TouchableOpacity } from 'react-native';
+import { Text, TextInput, View, FlatList, Image, TouchableOpacity, Picker } from 'react-native';
 
 export default class Kereses extends Component {
   constructor(props) {
     super(props);
     this.state = {
       cim: '',
+      filmmufaj: '',
+      filmmufajid:1,
+      services: [ 'one', 'two', 'three', 'four', 'five' ]
       
     };
   }
   
   
   componentDidMount(){
-    return fetch('http://172.16.0.29:3000/filmek')
+    fetch('http://172.16.0.29:3000/mufajok')
+      .then((response) => response.json())
+      .then((responseJson) => {
+
+        this.setState({
+          isLoading: false,
+          dataSource2: responseJson,
+        }, function(){
+
+        });
+
+      })
+      .catch((error) =>{
+        console.error(error);
+      });
+
+
+     fetch('http://172.16.0.29:3000/filmek')
       .then((response) => response.json())
       .then((responseJson) => {
 
@@ -27,7 +47,10 @@ export default class Kereses extends Component {
       .catch((error) =>{
         console.error(error);
       });
+
   }
+  
+  
 
   kereses=async () =>{
      let bemenet ={
@@ -59,6 +82,10 @@ export default class Kereses extends Component {
    }
 
   render() {
+    let serviceItems = this.state.services.map( (s, i) => {
+      return <Picker.Item key={i} value={s} label={s} />
+  });
+    
     return (
     <View style={{flex:1,backgroundColor:"#262626"}}>
       <View style={{padding: 10, alignItems:"center", backgroundColor:"#262626"}}>
@@ -69,6 +96,20 @@ export default class Kereses extends Component {
         onChangeText={(cim) => this.setState({cim})}
         value={this.state.cim}
         />
+
+        
+    <View style={{}}>
+                  <Picker
+                    selectedValue={this.state.selectedService}
+                    onValueChange={ (service) => ( this.setState({selectedService:service}) ) } >
+
+                    {serviceItems}
+
+                  </Picker>
+        </View>
+
+
+
 
         <TouchableOpacity 
           onPress={async ()=>this.kereses()}>
@@ -81,10 +122,10 @@ export default class Kereses extends Component {
           data={this.state.dataSource}      
           keyExtractor={({film_id}, index) => film_id}
           renderItem={({item}) =>
-          <View style={{justifyContent:"center",alignItems:"center"}}>
+          <View style={{justifyContent:"center",alignItems:"center", paddingBottom: 20, }}>
             <Image 
             source={{uri:'http://172.16.0.29:3000/'+item.film_kep}}
-            style={{width:175,height:250,margin:5,borderRadius:15}}
+            style={{width:175,height:250,margin:5,borderRadius:15,}}
             />
             <Text style={{color:"white",margin:5}}>{item.film_cim} ({item.film_ev})</Text>
    
