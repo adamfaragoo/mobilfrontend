9e34454c65791c49003f14b32bc7a11901e1c4f2
adamfaragoo <adam.farago004@gmail.com>
2022-01-06 13:34:16 +0100

    a

--- Kezdooldal.js
+++ Kezdooldal.js
@@ -44,26 +44,7 @@ export default class Filmek extends React.Component {
       
       <View style={{flex: 1, paddingTop:20,backgroundColor:"#262626", }}>
         
-        <FlatList 
-          showsHorizontalScrollIndicator={false}
-          data={this.state.dataSource}
-          horizontal
-          //numColumns={2}
-          keyExtractor={({film_id}, index) => film_id}
-          renderItem={({item}) =>
-          <View style={{justifyContent:"center",alignItems:"center", flexDirection:'column', flex:1,marginTop:400}}>
-            <Image 
-            source={{uri:'http://172.16.0.29:3000/'+item.film_kep}}
-            style={{width:175,height:250,margin:5,borderRadius:15}}
-            />
-            <Text style={{color:"white",margin:5}}>{item.film_cim}</Text>
-            
-          </View>
-  
-        }
-        />
-        
-
+     
         
       </View>
     );
--- header2.js
+++ header2.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import {StyleSheet, Text, View, TouchableOpacity, Modal, Button, show,} from 'react-native';
 import {MaterialIcons} from '@expo/vector-icons';
 import { NavigationContainer,DrawerActions } from '@react-navigation/native';
 import { Ionicons } from "@expo/vector-icons";
@@ -9,27 +9,52 @@ import { Ionicons } from "@expo/vector-icons";
 export default class Header2 extends React.Component{
     constructor(props) {
         super(props);
+        this.state={
+            show:false,
+        }
         
       }
 
-      
 
-      asd=async()=>{
-        alert("alert")
-    
-        }
     render(){
         return(
             <View style={{justifyContent:"center",alignItems:"center",flexDirection:"row",width:"132%"}}>
                 <View >
                 <Text style={{fontWeight:"bold",color:"white",letterSpacing:1,fontSize:25, }}>Kezdőoldal</Text>
                 </View>
-                <TouchableOpacity onPress={async()=>this.asd()}>
+
+                <TouchableOpacity onPress={()=>{this.setState({show:true})}}>
                 <View style={{paddingLeft:90}}>
                 <Ionicons name="information-circle-sharp" size={30} color="white" />
             </View>
             </TouchableOpacity>
+            <Modal
+            transparent={true}
+            visible={this.state.show}
+            animationType={"fade"}
+            >
+            <View style={{backgroundColor:"#000000aa", flex:1}}>
+                <View style={{backgroundColor:"white",margin:50, padding:20, borderRadius:10}}>
+                <Text style={{fontSize:20,}}>Modal text </Text>
+
+                <TouchableOpacity onPress={()=>{this.setState({show:false})}}>
+                <View style={{alignItems:"center", marginTop:70,}}>
+                <Ionicons name="close-circle" size={30} color="black" />
+            </View>
+            </TouchableOpacity>
+                </View>
+
+            </View>
+            </Modal>
             </View>
         );
     }
 }
+
+/*
+<TouchableOpacity onPress={async()=>this.asd()}>
+                <View style={{paddingLeft:90}}>
+                <Ionicons name="information-circle-sharp" size={30} color="white" />
+            </View>
+            </TouchableOpacity>
+*/
